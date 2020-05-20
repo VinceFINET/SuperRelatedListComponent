1450f41f3fa52d1a29c4eb2d1148bcdc6ae9f28f
Vincent FINET <vfinet@salesforce.com>
2020-05-20 12:38:06 +0200

    Create  SRLSuperRelatedList_CMPHelper.js

---
+++ src/aura/SRLSuperRelatedList_CMP/SRLSuperRelatedList_CMPHelper.js
@@ -0,0 +1,32 @@
+({
+    convertArrayOfObjectsToCSV : function(headers, rows) {
+        if (rows == null || !rows.length) {
+            return null;
+        }
+
+        const COLUMN_DIVIDER = ',';
+        const LINE_DIVIDER = '\n';
+       
+        let csvStringResult = 'sep='+COLUMN_DIVIDER+LINE_DIVIDER;
+        csvStringResult += headers.join(COLUMN_DIVIDER);
+        csvStringResult += LINE_DIVIDER;
+
+        console.debug('START csvStringResult=', csvStringResult);
+        for(let i=0; i < rows.length; i++){   
+            let counter = 0;
+            for(var header in headers) {
+                if (counter > 0) { 
+                    csvStringResult += COLUMN_DIVIDER; 
+                }
+                const fieldname = headers[header]; 
+                const fieldvalue = rows[i][fieldname] || '';
+                csvStringResult += '"'+ fieldvalue +'"'; 
+                counter++;
+            }
+            csvStringResult += LINE_DIVIDER;
+	        console.debug('LOOP i=', i, ', csvStringResult=', csvStringResult);
+        }
+        console.debug('END csvStringResult=', csvStringResult);
+        return csvStringResult;        
+    }
+})
