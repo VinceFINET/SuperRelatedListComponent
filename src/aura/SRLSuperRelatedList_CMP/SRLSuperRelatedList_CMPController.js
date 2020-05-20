a388f895f8e02757939a75c7d1addde50055b5c8
Vincent FINET <vfinet@salesforce.com>
2020-05-20 12:38:01 +0200

    Update SRLSuperRelatedList_CMPController.js

--- src/aura/SRLSuperRelatedList_CMP/SRLSuperRelatedList_CMPController.js
+++ src/aura/SRLSuperRelatedList_CMP/SRLSuperRelatedList_CMPController.js
@@ -1,32 +1,71 @@
 ({
-    convertArrayOfObjectsToCSV : function(headers, rows) {
-        if (rows == null || !rows.length) {
-            return null;
+    doInit: function(component, event, helper) {        
+        // ------------------------------------------
+        // Read "tableConfig" from configuration to generate table headers
+        // ------------------------------------------
+        const jsonTableConfig = component.get("v.tableConfig");
+		    const jsonCsvConfig = component.get("v.csvConfig");
+        try {
+            const tableConfig = JSON.parse(jsonTableConfig);
+            component.set("v.addFields", tableConfig.additionalFields);
+            component.set("v.tableColumns", tableConfig.columns);
+            const csvConfig = JSON.parse(jsonCsvConfig);
+            component.set("v.csvHeaders", csvConfig.headers);
+        } catch (error) {
+            console.error('tableConfig=', jsonTableConfig, 'csvConfig=', csvConfig, 'Error=', error);          
         }
-
-        const COLUMN_DIVIDER = ',';
-        const LINE_DIVIDER = '\n';
-       
-        let csvStringResult = 'sep='+COLUMN_DIVIDER+LINE_DIVIDER;
-        csvStringResult += headers.join(COLUMN_DIVIDER);
-        csvStringResult += LINE_DIVIDER;
-
-        console.debug('START csvStringResult=', csvStringResult);
-        for(let i=0; i < rows.length; i++){   
-            let counter = 0;
-            for(var header in headers) {
-                if (counter > 0) { 
-                    csvStringResult += COLUMN_DIVIDER; 
-                }
-                const fieldname = headers[header]; 
-                const fieldvalue = rows[i][fieldname] || '';
-                csvStringResult += '"'+ fieldvalue +'"'; 
-                counter++;
+    },
+    doDownloadCsv: function(component, event, helper) {
+        const headers = component.get("v.csvHeaders");
+        const rows = component.get("v.tableRows");
+        const csv = helper.convertArrayOfObjectsToCSV(headers, rows);
+        if (csv == null) return; 
+        
+        // ####--code for create a temp. <a> html tag [link tag] for download the CSV file--####     
+        const hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_self'; // 
+        hiddenElement.download = 'ExportData.csv';  // CSV file Name* you can change it.[only name not .csv] 
+        document.body.appendChild(hiddenElement); // Required for FireFox browser
+        hiddenElement.click(); // using click() js function to download csv file
+    },
+    doGetRows: function(component, event, helper) {
+        // ------------------------------------------
+        // Call the SOQL apex controller
+        // ------------------------------------------
+        const action = component.get("c.GetRows");
+        action.setParams({
+            "fields": component.get("v.soqlFields"),
+            "table":  component.get("v.soqlTable"),
+            "whereClause": component.get("v.soqlWhere"),
+            "orderByClause": component.get("v.soqlOrderBy"),
+            "variables": component.get("v.record")
+        });
+        action.setCallback(this, function(response){
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const rows = response.getReturnValue();
+                const addFields = component.get("v.addFields");
+                addFields.forEach(field => {
+                    rows.forEach(row => {
+                        // Workaround for lookups fields in datatable
+                        for (var f in row) if (typeof row[f] === 'object') {
+                            for (var relField in row[f]) {
+                               row[f + "." + relField] = row[f][relField];
+                            }
+                        }
+                        // Calculate additional fields
+                        let value = field.formula;
+                        field.params.forEach(p => value = value.replace('['+p+']', row[p]));
+                        row[field.name] = value;
+                	});
+                });
+                component.set("v.tableRows", rows);
+            } else {
+                console.error('State=', state);
+                console.error('Error=', response.getError()[0]);
             }
-            csvStringResult += LINE_DIVIDER;
-	        console.debug('LOOP i=', i, ', csvStringResult=', csvStringResult);
-        }
-        console.debug('END csvStringResult=', csvStringResult);
-        return csvStringResult;        
-    }
+        });
+        $A.enqueueAction(action);
+	}
 })
